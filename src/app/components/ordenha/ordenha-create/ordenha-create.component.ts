cbb1a7167806f2ec9e7dc75d3cd3bc80842e60a3
Bruno Henrique <brunohenrique2208sb@gmail.com>
2023-05-24 15:18:03 -0300

    Adicionando

--- src/app/components/ordenha/ordenha-create/ordenha-create.component.ts
+++ src/app/components/ordenha/ordenha-create/ordenha-create.component.ts
@@ -8,7 +8,10 @@ import { Tanque } from 'src/app/models/tanque';
 import { AnimalService } from 'src/app/services/animal.service';
 import { OrdenhaService } from 'src/app/services/ordenha.service';
 import { TanqueService } from 'src/app/services/tanque.service';
-
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import jsPDF from 'jspdf';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-ordenha-create',
   templateUrl: './ordenha-create.component.html',
@@ -92,7 +95,7 @@ export class OrdenhaCreateComponent implements OnInit {
         },
         (error) => {
           console.log('Erro ao cadastrar ordenha:', error);
-        }
+        }       
       );
     });
   }
@@ -104,7 +107,110 @@ export class OrdenhaCreateComponent implements OnInit {
       input.value = input.value.slice(0, -1);
     }
   }
+  gerarFichaPDF(): void {
+    const doc = new jsPDF();
+    const colWidth = 30;
+    const rowHeight = 10;
+    const margin = 10;
+    const pageWidth = doc.internal.pageSize.getWidth(); // Largura da página
+    const contentWidth = colWidth * 6; // Largura total do conteúdo (5 colunas)
   
+    let currentY = margin;
+  
+    const currentDate = new Date();
+    const days = [0, 1, 2]; // Gera para hoje, amanhã e depois de amanhã
+  
+    days.forEach((dayOffset, index) => {
+      const date = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + dayOffset);
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear().toString();
+      const data = `${day}/${month}/${year}`;
+  
+      // Título principal
+      doc.setFontSize(18);
+      doc.setFont('bold');
+      doc.setTextColor(0);
+      const title = 'FICHA DE ORDENHA';
+      const titleWidth = (pageWidth - contentWidth) / 2; // Espaçamento igual à esquerda e à direita
+      const titleX = (pageWidth - titleWidth) / 2; // Posição X para centralizar o título
+      doc.text(title, titleX, currentY + doc.getFontSize(), { align: 'center' });
+      currentY += rowHeight * 2;
+  
+      // Títulos das colunas
+      doc.setFontSize(14);
+      doc.setFont('bold');
+      doc.setDrawColor(0);
+  
+      const titleMargin = (pageWidth - contentWidth) / 2; // Espaçamento igual à esquerda e à direita
+  
+      doc.setTextColor(255);
+      doc.setFillColor(49, 102, 44);
+      doc.rect(titleMargin, currentY, colWidth, rowHeight, 'FD'); // Código
+      doc.text('Código', titleMargin + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+      doc.setTextColor(255);
+      doc.setFillColor(49, 102, 44);
+      doc.rect(titleMargin + colWidth, currentY, colWidth, rowHeight, 'FD'); // Apelido
+      doc.text('Apelido', titleMargin + colWidth + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+      doc.setTextColor(255);
+      doc.setFillColor(49, 102, 44);
+      doc.rect(titleMargin + colWidth * 2, currentY, colWidth, rowHeight, 'FD'); // 1ª Ordenha
+      doc.text('1ª Ordenha', titleMargin + colWidth * 2 + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+      doc.setTextColor(255);
+      doc.setFillColor(49, 102, 44);
+      doc.rect(titleMargin + colWidth * 3, currentY, colWidth, rowHeight, 'FD'); // 2ª Ordenha
+      doc.text('2ª Ordenha', titleMargin + colWidth * 3 + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+      doc.setTextColor(255);
+      doc.setFillColor(49, 102, 44);
+      doc.rect(titleMargin + colWidth * 4, currentY, colWidth, rowHeight, 'FD'); // Tanque
+      doc.text('Tanque', titleMargin + colWidth * 4 + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+      doc.setTextColor(255);
+      doc.setFillColor(49, 102, 44);
+      doc.rect(titleMargin + colWidth * 5, currentY, colWidth, rowHeight, 'FD'); // Data
+      doc.text('Data', titleMargin + colWidth * 5 + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+      currentY += rowHeight;
+  
+      // Preenchimento dos dados para cada animal
+      doc.setFont('normal');
+      doc.setDrawColor(0);
+      doc.setTextColor(0);
+      doc.setFillColor(255, 255, 255); // Cor branca para o preenchimento dos campos
+  
+      this.animais.forEach((animal) => {
+        doc.rect(titleMargin, currentY, colWidth, rowHeight); // Código
+        doc.text(animal.codigo.toString(), titleMargin + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+        doc.rect(titleMargin + colWidth, currentY, colWidth, rowHeight); // Apelido
+        doc.text(animal.apelido, titleMargin + colWidth + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+        doc.rect(titleMargin + colWidth * 2, currentY, colWidth, rowHeight); // 1ª Ordenha
+        doc.rect(titleMargin + colWidth * 3, currentY, colWidth, rowHeight); // 2ª Ordenha
+        doc.rect(titleMargin + colWidth * 4, currentY, colWidth, rowHeight); // Tanque
+  
+        doc.rect(titleMargin + colWidth * 5, currentY, colWidth, rowHeight); // Data
+        doc.text(data, titleMargin + colWidth * 5 + colWidth / 2, currentY + rowHeight / 2 + 2, { align: 'center' });
+  
+        currentY += rowHeight;
+        doc.setDrawColor(200);
+        doc.line(titleMargin, currentY, titleMargin + contentWidth, currentY); // Linha horizontal separando animais
+      });
+  
+      if (index !== days.length - 1) {
+        doc.addPage();
+        currentY = margin;
+      }
+    });
+  
+    doc.save('fichas_ordenha.pdf');
+  }
+  
+  
+
 
   cancel(): void {
     this.router.navigate(['/ordenha']);
